c1e220b7ece7f4590dd99659925022ca6c64ef49
nedazare <nedazare299@gmail.com>
2024-06-17 07:31:59 +0330

    about us page added

--- inc/functions/cyn-acf.php
+++ inc/functions/cyn-acf.php
@@ -10,6 +10,7 @@ function cyn_register_acf() {
 }
 
 function cyn_register_acf_company_settings() {
+	//فیلد پادکست ها
 	$fields = [ 
 		cyn_acf_add_text( 'time_podcast', 'زمان پادکست' ),
 
@@ -26,7 +27,7 @@ function cyn_register_acf_company_settings() {
 
 	cyn_register_acf_group( 'فیلدها', $fields, $location );
 
-
+	//فیلدهای صفحه اصلی
 	$field_home = [ 
 		cyn_acf_add_text( 'podcast_all_link', 'لینک مشاهده (همه پادکست ها)' ),
 		cyn_acf_add_text( 'blog_all_link', 'لینک مشاهده (همه مقالات)' ),
@@ -45,6 +46,27 @@ function cyn_register_acf_company_settings() {
 	];
 
 	cyn_register_acf_group( 'فیلدها', $field_home, $location_home );
+
+
+	//فیلدهای صفحه درباره ما
+	$field_home = [
+		cyn_acf_add_text( 'about_title_top', 'متن عنوان بالا' ),
+		cyn_acf_add_text( 'about_title_bottom', 'متن زیر عنوان صفحه' ),
+		cyn_acf_add_image( 'gallery_img_one', 'تصویر بزرگ سمت چپ' ),
+		cyn_acf_add_image( 'gallery_img_two', 'تصویر بالا سمت راست' ),
+		cyn_acf_add_image( 'gallery_img_three', 'تصویر پایین سمت راست' ),
+	];
+	$location_home = [ 
+		[ 
+			[ 
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'templates/about-template.php',
+			],
+		],
+	];
+
+	cyn_register_acf_group( 'گالری تصاویر', $field_home, $location_home );
 
 }
 
--- templates/about-template.php
+++ templates/about-template.php
@@ -2,14 +2,41 @@
 /* Template name: aboutus */
 get_header();
 
-$btn_link_whatsapp = get_option('btn_link_whatsapp');
-$btn_link_phones = get_option('btn_link_phones');
- ?>
- 
-<section class="bg-no-repeat items-center bg-contain md:bg-cover h-auto lg:h-[45rem] instagram-page" style="background-image:url('<?= get_stylesheet_directory_uri().'/assets/img/bg-templates.png'; ?>');">
-    <div class="container mx-auto flex h-full items-center justify-start px-4 lg:px-10 mt-">
-        
+$thumbnail_url = get_the_post_thumbnail_url();
+$about_title_top = get_field('about_title_top');
+$about_title_bottom = get_field('about_title_bottom');
+
+$gallery_img_one_id = get_field('gallery_img_one');
+$img_one = wp_get_attachment_image_src( $gallery_img_one_id, 'full' );
+
+$gallery_img_two_id = get_field('gallery_img_two');
+$img_two = wp_get_attachment_image_src( $gallery_img_two_id, 'full' );
+
+$gallery_img_three_id = get_field('gallery_img_three');
+$img_three = wp_get_attachment_image_src( $gallery_img_three_id, 'full' );
+?>
+<section class="about-us-page">
+    <div class="bg-no-repeat items-center bg-cover md:bg-cover h-auto lg:h-[45rem]" style="background-image:url('<?= $thumbnail_url; ?>');">
+        <div class="container mx-auto flex h-full items-center justify-end px-3 lg:px-10">
+            <div class="w-[60%] pt-16 pb-16 ">
+                <h1 class="text-white text-xl lg:text-5xl mb-4 leading-6 lg:leading-424 font-medium"><?= esc_html($about_title_top);?></h1>
+                <h3 class="text-white text-base lg:text-4xl font-medium"><?= esc_html($about_title_bottom);?></h3>
+            </div>
+        </div>
     </div>
+    <div class="container mx-auto px-4 lg:px-10 mt-10">
+        <?php the_content();?>
+
+        <h2 class="mt-12">گالری تصاویر</h2>
+        <div class="flex gap-4 flex-col lg:flex-row mx-auto gallery">
+            <div class="flex flex-col gap-4 w-[100%] lg:w-[30%]">
+                <img class="rounded-4xl w-full" src="<?=$img_two[0]; ?>" />
+                <img class="rounded-4xl w-full" src="<?=$img_three[0]; ?>" />
+            </div>
+            <img class="rounded-4xl w-[100%] lg:w-[70%]" src="<?=$img_one[0]; ?>" />
+        </div>
+    </div>
+
 </section>
 
 <?php get_footer(); ?>
