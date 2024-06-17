f05559091daa57a8a33faeee770660755be45890
amirtanazzoh <92968872+amirtanazzoh@users.noreply.github.com>
2024-06-17 14:42:14 +0330

    init amir branch

--- inc/functions/cyn-update-checker.php
+++ inc/functions/cyn-update-checker.php
@@ -3,13 +3,11 @@ require ( get_stylesheet_directory() . '/inc/libs/plugin-update-checker/plugin-u
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $updateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/cyandm/website/', //github theme
+	'https://github.com/cyandm/sorrad/', //github theme
 	get_stylesheet_directory(),
-	'cyandm' //theme slug
+	'sorrad' //theme slug
 );
 
 //Set the branch that contains the stable release.
 $updateChecker->setBranch( 'main' );
 
-//Optional: If you're using a private repository, specify the access token like this:
-//$updateChecker->setAuthentication('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');
